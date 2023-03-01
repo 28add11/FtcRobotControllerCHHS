7da5caf20f5513fd8ddc6c7495c81fd5e3e5b34b
28add11 <kerbal2346@gmail.com>
2023-02-28 19:32:25 -0700

    woops forgot to save part of the prev commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorDrive.java
@@ -136,12 +136,6 @@ public class colorSensorDrive extends LinearOpMode {
 
         //COLORSENSOR SETUP
 
-        // hsvValues is an array that will hold the hue, saturation, and value information.
-        float hsvValues[] = {0F,0F,0F};
-
-        // values is a reference to the hsvValues array.
-        final float values[] = hsvValues;
-
         // get a reference to the RelativeLayout so we can change the background
         // color of the Robot Controller app to match the hue detected by the RGB sensor.
         int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
